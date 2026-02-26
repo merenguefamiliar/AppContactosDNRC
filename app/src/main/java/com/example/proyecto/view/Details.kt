6bfd632ccdfa432ee4fa8cd6691a82466d7feb72
merenguefamiliar <merenguefamiliar@gmail.com>
2026-02-26 18:45:51 +0100

    botones llamar y whatsapp

--- app/src/main/java/com/example/proyecto/view/Details.kt
+++ app/src/main/java/com/example/proyecto/view/Details.kt
@@ -1,5 +1,7 @@
 package com.example.proyecto.view
 
+import android.content.Intent
+import android.net.Uri
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
@@ -10,15 +12,22 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import coil.compose.AsyncImage
 import com.example.proyecto.viewmodel.ContactViewModel
+import androidx.core.net.toUri
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun PantallaDetalles(navController: NavHostController, viewModel: ContactViewModel, contactoId: Int) {
+fun PantallaDetalles(
+    navController: NavHostController,
+    viewModel: ContactViewModel,
+    contactoId: Int
+) {
 
+    val context = LocalContext.current
     val contacto = viewModel.contactos.collectAsState().value.find { it.id == contactoId }
 
     if (contacto != null) {
@@ -45,7 +54,11 @@ fun PantallaDetalles(navController: NavHostController, viewModel: ContactViewMod
                     .padding(15.dp),
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                AsyncImage(model = contacto.imageUrl, contentDescription = "Foto del contacto", modifier = Modifier.size(300.dp))
+                AsyncImage(
+                    model = contacto.imageUrl,
+                    contentDescription = "Foto del contacto",
+                    modifier = Modifier.size(300.dp)
+                )
                 Spacer(Modifier.height(10.dp))
                 Text("Nombre: ${contacto.name}")
                 Text("Teléfono: ${contacto.phone}")
@@ -53,16 +66,40 @@ fun PantallaDetalles(navController: NavHostController, viewModel: ContactViewMod
 
                 Spacer(modifier = Modifier.height(8.dp))
 
-                Button( onClick = { viewModel.eliminarContact(contacto)
-                    navController.popBackStack() },
-                    colors = ButtonDefaults.buttonColors(Color.Red))
-                { Text("Eliminar contacto", color = Color.White)}
+                Button(
+                    onClick = {
+                        viewModel.eliminarContact(contacto)
+                        navController.popBackStack()
+                    },
+                    colors = ButtonDefaults.buttonColors(Color.Red)
+                )
+                { Text("Eliminar contacto", color = Color.White) }
 
-                Spacer(modifier = Modifier.height(8.dp))
+                Button(
+                    onClick = { navController.navigate("editar/${contacto.id}") },
+                    colors = ButtonDefaults.buttonColors(Color.Red)
+                )
+                { Text("Editar contacto", color = Color.White) }
 
-                Button( onClick = { navController.navigate("editar/${contacto.id}") },
-                    colors = ButtonDefaults.buttonColors(Color.Red))
-                { Text("Editar contacto", color = Color.White)}
+                Button(
+                    onClick = {
+                        val url = "https://wa.me/${contacto.phone}"
+                        val intent = Intent(Intent.ACTION_VIEW)
+                        intent.data = url.toUri()
+                        context.startActivity(intent)
+                    },
+                    colors = ButtonDefaults.buttonColors(Color.Red)
+                ) { Text("Compartir en WhatsApp") }
+
+                Button(
+                    onClick = {
+                        val intent = Intent(Intent.ACTION_DIAL).apply {
+                            data = "tel:${contacto.phone}".toUri()
+                        }
+                        context.startActivity(intent)
+                    },
+                    colors = ButtonDefaults.buttonColors(Color.Red)
+                ) { Text("Llamar") }
             }
 
         }
